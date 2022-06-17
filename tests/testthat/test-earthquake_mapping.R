ee7cb2a303b60481359fa970941f8a30076239e2
Hannah Liyanarachchi <hliyanar@yahoo.com>
2022-06-18 04:01:08 +1200

    Added tests

--- tests/testthat/test-earthquake_mapping.R
+++ tests/testthat/test-earthquake_mapping.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+data(eq_data)
+
+test_that("eq_create_label creates the correct vector",{
+  expect_type(eq_create_label(eq_raw_cleaner(as.data.frame(tail(eq_data)))),
+              "character")
+  expect_equal(length(eq_create_label(eq_raw_cleaner(as.data.frame(tail(eq_data))))),6)
+})
+
+test_that("eq_map produces the correct output", {
+  expect_s3_class(eq_map(eq_raw_cleaner(as.data.frame(tail(eq_data)))),
+                  class= 'leaflet')
 })
